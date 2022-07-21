79b9b572716164ade95de2f659635f9b0cc825bc
Addison Baum <70862672+addycb@users.noreply.github.com>
2022-07-21 15:26:35 -0400

    Revert "Merge branch 'main' of https://github.com/addycb/Tanning-Scheduler"
    This reverts commit 25d402d9844a21f103357ef6a5f867e110b0a9f1, reversing
    changes made to 0b440a522ce8053a226b9614f54f81afb1ab819f.

--- app.py
+++ app.py
@@ -41,60 +41,43 @@ users = cursor.fetchall()
 
 
 def getUvi(lat, lon, exclude="minutely,current,alerts"):
-    # Takes coordinates (ints), returns tuple of two lists: uv index of next 48 hours and 5 days.
+	#Takes coordinates (ints), returns tuple of two lists: uv index of next 48 hours and 5 days.
     apikey = os.environ.get('apikey')
-    api_url = "https://api.openweathermap.org/data/3.0/onecall?lat={0}&lon={1}&exclude={2}&appid={3}".format(
-        lat, lon, exclude, apikey)
-    response = requests.get(api_url)
-    response = response.json()
-    hours = []
-    days = []
+    api_url = "https://api.openweathermap.org/data/3.0/onecall?lat={0}&lon={1}&exclude={2}&appid={3}".format(lat, lon, exclude, apikey)
+    response=requests.get(api_url)
+    response=response.json()
+    hours=[]
+    days=[]
     for x in response["hourly"]:
-        dt = x["dt"]
-        uvi = x["uvi"]
-        hours.append(dt, uvi)
+        dt=x["dt"]
+        uvi=x["uvi"]
+        hours.append(dt,uvi)
     print("daily")
     for x in range(5):
-        y = response["daily"][x]
-        dt = y["dt"]
-        uvi = y["uvi"]
-        days.append((dt, uvi))
-    return (hours, days)
-
+        y=response["daily"][x]
+        dt=y["dt"]
+        uvi=y["uvi"]
+        days.append((dt,uvi))
+    return (hours,days)
 
 def getLocation(user):
-    # Gets location of user from database, if not found returns False
-    cursor = conn.cursor()
-    if cursor.execute("SELECT lat,lon from zipcodes where zipcode in(select zipcode from users where email='{0}')".format(user)):
-        return cursor.fetchone()
-    else:
-        cursor.execute(
-            "Select zipcode from users where email='{0}'".format(user))
-        zipcode = cursor.fetchone()
-        latlon = getlatLon(zipcode)
-        cursor.execute("Insert INTO zipcodes (zipcode,lat,lon) VALUES (%s,%s,%s)",
-                       zipcode, latlon[0], latlon[1])
-        conn.commit()
-        return latlon
-
-def getTimeZone(latlon):
-    key=str(os.environ.get('timezonekey'))  
-    url="http://api.timezonedb.com/v2.1/get-time-zone?key={0}&format=json&by=position&lat={1}&lng=2".format(
-        key,latlon[0],latlon[1]
-    )
-    response=requests.get(url)
-    response=response.json()
-    return response["gmtOffset"]
+	#Gets location of user from database, if not found returns False
+	cursor=conn.cursor()
+	if cursor.execute("SELECT lat,lon from zipcodes where zipcode in(select zipcode from users where email='{0}')".format(user)):
+		return cursor.fetchone()
+	else:
+		cursor.execute("Select zipcode from users where email='{0}'".format(user))
+		zipcode=cursor.fetchone()
+		latlon=getlatLon(zipcode)
+		cursor.execute("Insert INTO zipcodes (zipcode,lat,lon) VALUES (%s,%s,%s)",zipcode,latlon[0],latlon[1])
+		conn.commit()
+		return latlon
 
 
-
-
-# Starter code for finding schedule. Takes user, uv preferences, schedule, outputs uv schedule data
-def getSchedule(useremail, schedule=None):
-    location = getLocation(useremail)
+#Starter code for finding schedule. Takes user, uv preferences, schedule, outputs uv schedule data
+def getSchedule(useremail,preferences,schedule=None):
+    location=getLocation(useremail)
     return 0
-
-
 '''
 	if location:
 		cursor=conn.cursor()
@@ -103,71 +86,58 @@ def getSchedule(useremail, schedule=None):
 		return False 
 '''
 
-
 def getUserList():
-    cursor = conn.cursor()
-    cursor.execute("SELECT email from Users")
-    return cursor.fetchall()
-
+	cursor = conn.cursor()
+	cursor.execute("SELECT email from Users")
+	return cursor.fetchall()
 
 def isEmailUnique(email):
-    # use this to check if a email has already been registered
-    cursor = conn.cursor()
-    if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
-        # this means there are greater than zero entries with that email
-        return False
-    else:
-        return True
+	#use this to check if a email has already been registered
+	cursor = conn.cursor()
+	if cursor.execute("SELECT email  FROM Users WHERE email = '{0}'".format(email)):
+		#this means there are greater than zero entries with that email
+		return False
+	else:
+		return True
 
 
 def getlatLon(zip):
     country = pgeocode.Nominatim('us')
     query = country.query_postal_code(zip)
-    latlon = (query["latitude"], query["longitude"])
+    latlon=(query["latitude"],query["longitude"])
     return latlon
 
-def getOffset(lat,lon):
-    key=str(os.environ.get('timezonekey'))  
-    url="http://api.timezonedb.com/v2.1/get-time-zone?key={0}&format=json&by=position&lat={1}&lng={2}".format(
-        key,lat,lon
-    )
-    response=requests.get(url)
-    response=response.json()
-    offset=int(response["gmtOffset"])
-    offset=offset/3600
-    return offset
 
-# end login code
+
+#end login code
 
 class User(flask_login.UserMixin):
-    pass
-
+	pass
 
 @login_manager.user_loader
 def user_loader(email):
-    users = getUserList()
-    if not(email) or email not in str(users):
-        return
-    user = User()
-    user.id = email
-    return user
+	users = getUserList()
+	if not(email) or email not in str(users):
+		return
+	user = User()
+	user.id = email
+	return user
 
 
 @login_manager.request_loader
 def request_loader(request):
-    users = getUserList()
-    email = request.form.get('email')
-    if not(email) or email not in str(users):
-        return
-    user = User()
-    user.id = email
-    cursor = mysql.connect().cursor()
-    cursor.execute(
-        "SELECT password FROM Users WHERE email = '{0}'".format(email))
-    data = cursor.fetchall()
-    pwd = str(data[0][0])
-    user.is_authenticated = request.form['password'] == pwd
-    return user
+	users = getUserList()
+	email = request.form.get('email')
+	if not(email) or email not in str(users):
+		return
+	user = User()
+	user.id = email
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email))
+	data = cursor.fetchall()
+	pwd = str(data[0][0])
+	user.is_authenticated = request.form['password'] == pwd
+	return user
 
 
 '''
@@ -180,8 +150,8 @@ def new_page_function():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if flask.request.method == 'GET':
-        return '''
+	if flask.request.method == 'GET':
+		return '''
 			   <form action='login' method='POST'>
 				<input type='text' name='email' id='email' placeholder='email'></input>
 				<input type='password' name='password' id='password' placeholder='password'></input>
@@ -189,68 +159,67 @@ def login():
 			   </form></br>
 		   <a href='/'>Home</a>
 			   '''
-    # The request method is POST (page is recieving data)
-    email = flask.request.form['email']
-    cursor = conn.cursor()
-    # check if email is registered
-    if cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email)):
-        data = cursor.fetchall()
-        pwd = str(data[0][0])
-        if flask.request.form['password'] == pwd:
-            user = User()
-            user.id = email
-            flask_login.login_user(user)  # okay login in user
-            # protected is a function defined in this file
-            return flask.redirect(flask.url_for('protected'))
+	# The request method is POST (page is recieving data)
+	email = flask.request.form['email']
+	cursor = conn.cursor()
+	# check if email is registered
+	if cursor.execute("SELECT password FROM Users WHERE email = '{0}'".format(email)):
+		data = cursor.fetchall()
+		pwd = str(data[0][0])
+		if flask.request.form['password'] == pwd:
+			user = User()
+			user.id = email
+			flask_login.login_user(user)  # okay login in user
+			# protected is a function defined in this file
+			return flask.redirect(flask.url_for('protected'))
 
-    # information did not match
-    return "<a href='/login'>Try again</a>\
+	# information did not match
+	return "<a href='/login'>Try again</a>\
 			</br><a href='/register'>or make an account</a>"
 
 
 @app.route('/logout')
 def logout():
-    flask_login.logout_user()
-    return render_template('hello.html', message='Logged out')
+	flask_login.logout_user()
+	return render_template('hello.html', message='Logged out')
 
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
-    return render_template('unauth.html')
+	return render_template('unauth.html')
 
 # you can specify specific methods (GET/POST) in function header instead of inside the functions as seen earlier
 
 
 @app.route("/register", methods=['GET'])
 def register():
-    return render_template('register.html', supress='True')
+	return render_template('register.html', supress='True')
 
 
 @app.route("/register", methods=['POST'])
 def register_user():
-    try:
-        email = request.form.get('email')
-        name = request.form.get('name')
-        password = request.form.get('password')
-        zipcode = request.form.get('zipcode')
-    except:
-        # this prints to shell, end users will not see this (all print statements go to shell)
-        print("couldn't find all tokens")
-        return flask.redirect(flask.url_for('register'))
-    cursor = conn.cursor()
-    test = isEmailUnique(email)
-    if test:
-        print(cursor.execute("INSERT INTO Users (email, name, password, zipcode) VALUES (%s, %s,%s,%s)",
-              (email, name, password, zipcode)))
-        conn.commit()
-        # log user in
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return render_template('hello.html', name=email, message='Account Created!')
-    else:
-        print("couldn't find all tokens")
-        return flask.redirect(flask.url_for('register'))
+	try:
+		email=request.form.get('email')
+		name=request.form.get('name')
+		password=request.form.get('password')
+		zipcode=request.form.get('zipcode')
+	except:
+		print("couldn't find all tokens") #this prints to shell, end users will not see this (all print statements go to shell)
+		return flask.redirect(flask.url_for('register'))
+	cursor = conn.cursor()
+	test =  isEmailUnique(email)
+	if test:
+		print(cursor.execute("INSERT INTO Users (email, name, password, zipcode) VALUES (%s, %s,%s,%s)",(email, name, password,zipcode)))
+		conn.commit()
+		#log user in
+		user = User()
+		user.id = email
+		flask_login.login_user(user)
+		return render_template('hello.html', name=email, message='Account Created!')
+	else:
+		print("couldn't find all tokens")
+		return flask.redirect(flask.url_for('register'))
+
 
 
 # default page
@@ -258,30 +227,26 @@ def register_user():
 
 @app.route("/", methods=['GET'])
 def hello():
-    return render_template('hello.html')
+	return render_template('hello.html')
 
 
 @app.route("/scheduler", methods=['POST'])
 @flask_login.login_required
 def calendar():
-    # gets user inputs, redirects to page with schedule
-    try:
-        try:
-            schedule=request.form.get('schedule')
-        except:
-            pass
-        useremail = flask_login.current_user.id
-    except:
-        # this prints to shell, end users will not see this (all print statements go to shell)
-        print("couldn't find all tokens")
-    schedule = getSchedule(useremail,schedule)
-    if(schedule):
-        pass
-    else:
-        print("Error")
-
+	#gets user inputs, redirects to page with schedule
+	try:
+		preferences=request.form.get('preferences')	
+		schedule=request.form.get('schedule')
+		useremail=flask_login.current_user.id
+	except:
+		print("couldn't find all tokens") #this prints to shell, end users will not see this (all print statements go to shell)
+	schedule=getSchedule(useremail,preferences,schedule)
+	if(schedule):
+		pass
+	else:
+		print("Error")
 
 if __name__ == "__main__":
-    # this is invoked when in the shell  you run
-    # $ python app.py
-    app.run(port=5000, debug=True)
+	# this is invoked when in the shell  you run
+	# $ python app.py
+	app.run(port=5000, debug=True)
